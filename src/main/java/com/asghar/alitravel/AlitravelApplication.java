b7d2bb32d7c8b347e4997fd1b6ef36e00d6c94ba
Mohd-Asghar <mohdasghar10234@gmail.com>
2024-04-24 13:02:51 +0530

    boot leaning

---
+++ src/main/java/com/asghar/alitravel/AlitravelApplication.java
@@ -0,0 +1,13 @@
+package com.asghar.alitravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlitravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlitravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asghar/alitravel/AlitravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asghar.alitravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlitravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
